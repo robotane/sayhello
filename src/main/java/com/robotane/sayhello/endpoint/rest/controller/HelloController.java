6fd3769717443b436092a435ab5e2c6d303b8223
John Robotane <32599378+robotane@users.noreply.github.com>
2025-02-23 08:19:31 +0400

    Update HelloController.java

--- src/main/java/com/robotane/sayhello/endpoint/rest/controller/HelloController.java
+++ src/main/java/com/robotane/sayhello/endpoint/rest/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.robotane.sayhello.endpoint.rest.controller;
 
+import com.robotane.sayhello.service.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
